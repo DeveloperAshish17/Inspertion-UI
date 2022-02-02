de6bbd0728fda2a7da4ebef0cedebbf6fdfd8b5c
developerashish17 <yadavashish0798@gmail.com>
2022-02-02 12:47:19 +0530

    updated

--- src/app/inspection/adit-inspection/adit-inspection.component.ts
+++ src/app/inspection/adit-inspection/adit-inspection.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MasterService } from 'src/app/services/master.service';
 
 @Component({
   selector: 'app-adit-inspection',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AditInspectionComponent implements OnInit {
 
-  constructor() { }
+  inspectionList$!: Observable<any[]>;
+  statusList$!: Observable<any[]>;
+  inspectionTypesList$!: Observable<any[]>;
+
+  @Input() inspection: any;
+  id: number = 0;
+  status: string = "";
+  comments: string = "";
+  inspectionTypeId!: number;
+
+  constructor(private master: MasterService) { }
 
   ngOnInit(): void {
+    this.id = this.inspection.id;
+    this.status = this.inspection.status;
+    this.comments = this.inspection.comments;
+    this.inspectionTypeId = this.inspectionTypeId;
+    this.statusList$ = this.master.getStatusList();
+    this.inspectionList$ = this.master.getInspectionList();
+    this.inspectionTypesList$ = this.master.getInspectionTypesList();
+  }
+
+  addInspection() {
+    var inspection = {
+      status: this.status,
+      comments: this.comments,
+      inspectionTypeId: this.inspectionTypeId
+    }
+    this.master.addInspection(inspection).subscribe(res => {
+      var closeModelBtn = document.getElementById('adit-model-close');
+      if (closeModelBtn) {
+        closeModelBtn.click();
+      }
+
+      var showAddSucess = document.getElementById('add-success-alert');
+      if (showAddSucess) {
+        showAddSucess.style.display = "block";
+      }
+      setTimeout(function () {
+        if (showAddSucess) {
+          showAddSucess.style.display = "none"
+
+        }
+      }, 5000);
+    })
+  }
+
+  updateInspection() {
+
   }
 
 }
--- src/app/inspection/show-inspection/show-inspection.component.ts
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -26,12 +26,32 @@ export class ShowInspectionComponent implements OnInit {
 
   }
 
+  //variable (properties)
+  modelTitle: string = '';
+  activateAditInspectionComponent: boolean = false;
+  inspection: any;
+
+  modelAdd() {
+    this.inspection = {
+      id: 0,
+      status: null,
+      Comment: null,
+      inspectionTypeId: null
+    }
+    this.modelTitle = "Add Inspection";
+    this.activateAditInspectionComponent = true;
+  }
+
+  modelClose() {
+    this.activateAditInspectionComponent = false;
+    this.inspectionList$ = this.master.getInspectionList();
+  }
+
   refreshInspectionTypesMap() {
     this.master.getInspectionTypesList().subscribe(data => {
       this.inspectionTypeList = data;
 
-      for(let i = 0; i < data.length; i++)
-      {
+      for (let i = 0; i < data.length; i++) {
         this.inspectionTypeMap.set(this.inspectionTypeList[i].id, this.inspectionTypeList[i].inspectionName);
       }
     })
